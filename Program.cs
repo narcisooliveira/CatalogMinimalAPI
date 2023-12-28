ebc1fb84d59cb2e5485771475ba3e5e9682eec32
narcisooliveira <nrcsfo@gmail.com>
2023-12-28 11:54:10 -0300

    Definindo string de conexão com o BD e configurando serviço de conexão na classe Program.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using CatalogMinimalAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<CatalogContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
