62cb139d046ca653f05e9a570760ef515127f8b2
narcisooliveira <nrcsfo@gmail.com>
2023-12-28 10:56:13 -0300

    Add project files.

---
+++ Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogMinimalAPI.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public List<Produto>? Produtos { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace CatalogMinimalAPI.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string? ImagemUrl { get; set; }
+        public DateTime DataCompra { get; set; }
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+    }
+}
